87180f4d10658305ae80db8a98768c6cd719ea25
Nicolás Gatica <nicolasgatica@macbook-pro-de-nicolas.local>
2019-06-20 19:47:47 -0400

    Initial Commit

---
+++ icon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  icon
+//
+//  Created by Nicolás Gatica on 6/20/19.
+//  Copyright © 2019 Blusigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
